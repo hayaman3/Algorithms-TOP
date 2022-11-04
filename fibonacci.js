daf4e0966e373777957295899c886e93fe7c661f
hayaman3 <kevin.haya@yahoo.com>
2022-11-05 00:18:53 +0800

    Added linkedList.js

--- fibonacci.js
+++ fibonacci.js
@@ -3,52 +3,37 @@
 // Using an example input of 8,
 // this method should return the array [0, 1, 1, 2, 3, 5, 8, 13].
 
-// function fib(n) {
-// 	let a = 1;
-// 	let b = 1;
-// 	let arr = [0, 1];
-// 	if (n === 0) return;
-// 	if (n === 1) return [0];
-// 	for (let i = 3; n > i; i++) {
-// 		console.log(i);
-// 		let c = a + b;
-// 		arr.push(c);
-// 		a = b;
-// 		b = c;
-// 	}
-// 	return arr;
-// }
+function fib(n) {
+	let a = 1;
+	let b = 1;
+	let arr = [0, 1];
+	if (n === 0) return;
+	if (n === 1) return [0];
+	for (let i = 3; n > i; i++) {
+		console.log(i);
+		let c = a + b;
+		arr.push(c);
+		a = b;
+		b = c;
+	}
+	return arr;
+}
 
-// console.log(fib(8)); // returns  [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(fib(8)); // returns  [0, 1, 1, 2, 3, 5, 8, 13]
 
 // Now write another method fibsRec which solves the same problem recursively.
 // This can be done in just a couple of lines
 // (or 1 if you’re crazy, but don’t consider either of these lengths a requirement…
 // just get it done).
 
-// function fibsRec(n) {
-// 	if (n === 0) return null;
-// 	if (n === 1) return [0];
-// 	if (n === 2) return [0, 1];
-// 	else {
-// 		let arr = fibsRec(n - 1);
-// 		arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
-// 		return arr;
-// 	}
-// }
-// console.log(fibsRec(8)); // returns  [0, 1, 1, 2, 3, 5, 8, 13]
-let arr = [0, 1];
-function fibsRecTernary(n) {
-	return n < 1
-		? null
-		: n < 2
-		? [0, 1]
-		: arr.push(fibsRecTernary(arr.length - 1) + fibsRecTernary(arr.length - 2));
+function fibsRec(n) {
+	if (n === 0) return null;
+	if (n === 1) return [0];
+	if (n === 2) return [0, 1];
+	else {
+		let arr = fibsRec(n - 1);
+		arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+		return arr;
+	}
 }
-
-// // return condition1 ? value1
-// // : condition2 ? value2
-// // : condition3 ? value3
-// // : value4;
-
-console.log(fibsRecTernary(8));
+console.log(fibsRec(8)); // returns  [0, 1, 1, 2, 3, 5, 8, 13]
